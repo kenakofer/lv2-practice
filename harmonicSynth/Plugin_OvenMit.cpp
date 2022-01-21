0e9a0f9bce954bd83af51b385343844811e7d056
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-01-21 14:07:47 -0500

    Haven't tested, but unity plugin code mostly ready

--- harmonicSynth/Plugin_OvenMit.cpp
+++ harmonicSynth/Plugin_OvenMit.cpp
@@ -1,7 +1,12 @@
 #include "AudioPluginUtil.h"
+#include "Key.hpp"
+#include "Filter2.hpp"
 
 namespace OvenMit
 {
+    const int MAX_KEYS = 1;
+    const int MAX_INSTANCES = 32;
+
     enum Param
     {
         P_ENV_MODE_1 = 0,
@@ -28,23 +33,34 @@ namespace OvenMit
         P_WAVEFORM_2 = 18,
         P_LEVEL_2    = 19,
         P_PITCH_2    = 20,
-        P_NUM         = 21
+        P_INSTANCE   = 21,
+        P_NUM         = 22
     };
 
     struct EffectData
     {
-        struct Data
-        {
-            float p[P_NUM];
-            float s;
-            float c;
-        };
-        union
-        {
-            Data data;
-            unsigned char pad[(sizeof(Data) + 15) & ~15]; // This entire structure must be a multiple of 16 bytes (and and instance 16 byte aligned) for PS3 SPU DMA requirements
-        };
+        float parameters[P_NUM];
     };
+
+    struct OvenMitInstance
+    {
+        Key keys[MAX_KEYS];
+        Controls controls;
+        Filter filter;
+    };
+
+    inline OvenMitInstance* GetOvenMitInstance(int index)
+    {
+        static bool initialized[MAX_INSTANCES] = { false };
+        static OvenMitInstance instance[MAX_INSTANCES];
+        if (index < 0 || index >= MAX_INSTANCES)
+            return NULL;
+        if (!initialized[index])
+        {
+            initialized[index] = true;
+        }
+        return &instance[index];
+    }
 
     int InternalRegisterEffectDefinition(UnityAudioEffectDefinition& definition)
     {
@@ -75,6 +91,82 @@ namespace OvenMit
         RegisterParameter(definition, "Level2", "", 0.000f, 1.0f, 0.100f, 1.0f, 1.0f, P_LEVEL_2, "Fader level for oscillator 2");
         RegisterParameter(definition, "Pitch2", "", -24.0f, 24.0f, -12.0f, 1.0f, 1.0f, P_PITCH_2, "Pitch offset in semitones of oscillator 2");
 
+        RegisterParameter(definition, "Instance", "", 0.0f, (float)(MAX_INSTANCES-1), 0.0f, 1.0f, 1.0f, P_INSTANCE, "Which synth instance to communicate with");
+
         return numparams;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ProcessCallback(UnityAudioEffectState* state, float* inbuffer, float* outbuffer, unsigned int length, int inchannels, int outchannels)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        OvenMitInstance* instance = GetOvenMitInstance((int)data->parameters[P_INSTANCE]);
+        for (unsigned int n = 0; n < length; n++)
+        {
+            for (int i = 0; i < outchannels; i++)
+            {
+                float value = instance->keys[0].get();
+                outbuffer[n * outchannels + i] = value;
+            }
+            instance->keys[0].proceed();
+        }
+
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK CreateCallback(UnityAudioEffectState* state)
+    {
+        EffectData* effectdata = new EffectData;
+        memset(effectdata, 0, sizeof(EffectData));
+        for (int n = 0; n < MAX_KEYS; n++) {
+            // TODO any initialization needed here?
+            // effectdata->keys[n].Init();
+        }
+        state->effectdata = effectdata;
+        InitParametersFromDefinitions(InternalRegisterEffectDefinition, effectdata->parameters);
+        return UNITY_AUDIODSP_OK;
+    }
+
+    /* API functions */
+
+    extern "C" UNITY_AUDIODSP_EXPORT_API void OvenMit_TestKeyPress(int index) {
+        OvenMitInstance* instance = GetOvenMitInstance(index);
+        instance->keys[0].press(69, 127, &(instance->controls), &(instance->filter));
+    }
+
+    ////////////////////////////////////////////
+    /* Boilerplate copied from Unity examples */
+    ////////////////////////////////////////////
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ReleaseCallback(UnityAudioEffectState* state)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        delete data;
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK SetFloatParameterCallback(UnityAudioEffectState* state, int index, float value)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        if (index >= P_NUM)
+            return UNITY_AUDIODSP_ERR_UNSUPPORTED;
+        data->parameters[index] = value;
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK GetFloatParameterCallback(UnityAudioEffectState* state, int index, float* value, char *valuestr)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        if (index >= P_NUM)
+            return UNITY_AUDIODSP_ERR_UNSUPPORTED;
+        if (value != NULL)
+            *value = data->parameters[index];
+        if (valuestr != NULL)
+            valuestr[0] = 0;
+        return UNITY_AUDIODSP_OK;
+    }
+
+    int UNITY_AUDIODSP_CALLBACK GetFloatBufferCallback(UnityAudioEffectState* state, const char* name, float* buffer, int numsamples)
+    {
+        return UNITY_AUDIODSP_OK;
     }
 }
